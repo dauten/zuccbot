81dd68b048ca0206a1b7b71f0904cd6880376d69
Dale Auten <dauten96@gmail.com>
2018-12-21 23:08:54 -0600

    added codenames

--- botty2.py
+++ botty2.py
@@ -1,3 +1,4 @@
+#version 1.1.1
 import discord
 import os
 import sys
@@ -13,26 +14,31 @@ import pickle
 import urllib.request
 from forecastiopy import *
 import re
-from secret import BOT_SECRET, DARKSKY_KEY
 
 client = discord.Client()
 calendar = []
 users = []
 started = False
-
+shitpostingstreet = "405834555392655361"
 coin = ["Heads","Tails"]
+dank = 0
 
 #these 5 variables are all used with voice functionality
 voice = None
 player = None
 _in_voice = False
 _skip_flag = False
-q = queue.Queue(maxsize=0)	
+q = queue.Queue(maxsize=0)
 
 #coords for SIUE and api key for forecastio/darksky
-api_key = DARKSKY_KEY
+api_key = "afdcb714b448f3f390bf6da777457d43"
 lat = 38.792
 lon = -89.999
+
+board = []
+teamA = [] #list of user id's in team A
+teamB = [] #list of user id's in team B
+wordStates = [] #list of states for the corresponding word in board.  0=red, 1=blue, 2=gray, 3=black
 
 #will return 12 hour forecast.  currently inaccurate and therefore in progress
 async def weather():
@@ -62,7 +68,7 @@ async def weather():
 			output+=(str(int(hourly.get_hour(i)['humidity']*100)) + "% humidity\n")
 
 		output += "Accurate within +/-2U.  Brought to you by Carls Jr and Darksky"
-		return(output)	
+		return(output)
 
 
 #this function is passed a message and decides what all to do with it.  Details inline.
@@ -70,7 +76,7 @@ async def public(message):
 
 	#print content to console for my records
 	print(str(message.author.name) + " in " + str(message.channel.name) +" said \n\t" + ascii(message.content))
-	
+
 	author = message.author
 	global saved
 	global _skip_flag
@@ -78,13 +84,24 @@ async def public(message):
 	saved = message.channel
 	global player
 	global session
-	
+	global dank
+	global board
+	#check how dank we are, if we've received too many shitposts we need to inspire those godless heathens
+	if message.channel is client.get_channel(shitpostingstreet):
+		dank+=1
+	if dank > 6:
+		dank=0
+		time.sleep(random.randint(600,18000))
+		outputtext = subprocess.check_output(["wget","-qO-","http://inspirobot.me/api?generate=true"])
+		await client.send_message(message.channel, outputtext.decode("utf-8"))
+
+
 	if message.content.lower().startswith("~play"):
 		if message.author.voice_channel != None:
 			await play(message)
 		else:
 			await client.send_message(message.channel, "You must be in a voice channel to use voice commands")
-	elif message.content.lower().startswith("~skip"):		
+	elif message.content.lower().startswith("~skip"):
 		if message.author.voice_channel != None:
 			_skip_flag = True
 		else:
@@ -92,13 +109,13 @@ async def public(message):
 	elif message.content.lower().startswith("~queue"):
 		if message.channel != None:
 			q.put(message)
-			await client.send_message(message.channel, "Enqueued "+message.content[6:])	
+			await client.send_message(message.channel, "Enqueued "+message.content[6:])
 	elif message.content.lower().startswith("~qlist"):
 		if q.empty():
 			await client.send_message(message.channel,"No songs in queue.")
 		else:
-			outp = "Requested Songs:\n"		
-			tq = queue.Queue(maxsize = 0)		
+			outp = "Requested Songs:\n"
+			tq = queue.Queue(maxsize = 0)
 			while not q.empty():
 				temp = q.get()
 				outp = outp + temp.content[6:] + "\n"
@@ -127,11 +144,11 @@ async def public(message):
 		output = await weather()
 		await client.send_message(message.channel, output)
 	elif message.content.startswith("WAKE ME UP"):
-		await client.send_message(message.channel, "(CAN'T WAKE UP)")		
+		await client.send_message(message.channel, "(CAN'T WAKE UP)")
 	elif "wetback" in message.content.lower():
 		await client.send_message(message.channel, "o7")
 	elif message.content.lower().startswith("~inspire"):
-		#this uses wget, a linux terminal command to get an inspirational image.  The URL seen below, if plugged into a browser, will give the URL of a new generated image.  wget is a command to access and return contents of a webpage.  When this line is ran outputtext will be the URL of a new image, so we make sure its decoded correctly then post it.  Discord will embed the image automatically, but in the future I would like to have the bot post the actual image, instead of relying on Discord's embed. 
+		#this uses wget, a linux terminal command to get an inspirational image.  The URL seen below, if plugged into a browser, will give the URL of a new generated image.  wget is a command to access and return contents of a webpage.  When this line is ran outputtext will be the URL of a new image, so we make sure its decoded correctly then post it.  Discord will embed the image automatically, but in the future I would like to have the bot post the actual image, instead of relying on Discord's embed.
 		outputtext = subprocess.check_output(["wget","-qO-","http://inspirobot.me/api?generate=true"])
 		await client.send_message(message.channel, outputtext.decode("utf-8"))
 	elif message.content.count("(╯°□°）╯︵ ┻━┻") > 0:
@@ -197,7 +214,7 @@ async def public(message):
 		f.close()
 		session = True
 		while session:
-			f = open("map.txt", "r+")	
+			f = open("map.txt", "r+")
 			time.sleep(2)
 			await client.edit_message(msg, f.read())
 	elif message.content.lower().startswith("~endmap"):
@@ -213,26 +230,99 @@ async def public(message):
 	elif message.content.lower().startswith("~clearq"):
 		await rmq(message)
 	elif message.content.lower().startswith("~game"):
-		await client.change_presence(game=discord.Game(name=message.content[6:]))
+		await client.send_message(message.channel, "Oh nice, I love playing "+message.content[6:]+"!")
+		mygame = discord.Game(name=message.content[6:])
+		await client.change_status(game=mygame)
+	elif message.content.lower().startswith("~crash"):
+		print("Exiting...\n\n")
+		sys.exit()
+	elif message.content.lower().startswith("~codename"):
+		gamestart = True
+		wordlist = open("words.txt", "r").read()
+		board = []
+		for a in range(0, 36):
+			r = random.randint(0, len(wordlist.split("\n"))-1 )
+			board.append('{word: <14}'.format(word=wordlist.split("\n")[r])[:-2]+"  ")
+
+		#playing field initialized, post it
+		outstring = ""
+		c = 0
+		for instance in board:
+			outstring += instance
+			c += 1
+			if c > 5:
+				c = 0
+				outstring += "\n"
+		await client.send_message(message.channel, outstring)
+
+		# POPULATE wordStates
+
+	elif message.content.lower().startswith("~flip"):
+               #playing field initialized, post it
+		for each in range(0, len(board)):
+			if board[each].split(" ")[0].lower() == message.content.lower().split(" ")[1]:
+				if board[each][0] != "*":
+					if wordStates[each] == 0:
+						client.send_message(message.channel, "That card was Red")
+						board[each] = "*"+board[each].split(" ", 1)[0]+"*"+board[each].split(" ", 1)[1]
+					if wordStates[each] == 1:
+						client.send_message(message.channel, "That card was Blue")
+						board[each] = "*"+board[each].split(" ", 1)[0]+"*"+board[each].split(" ", 1)[1]
+					if wordStates[each] == 2:
+						client.send_message(message.channel, "That card was a civilian.  It is now the other teams turn.")
+						board[each] = "~~"+board[each].split(" ", 1)[0]+"~~"+board[each].split(" ", 1)[1]
+					if wordStates[each] == 3:
+						client.send_message(message.channel, "That card was the Assassin.  Game over.  You lose")
+						break
+				else:
+					client.send_message(message.channel, "That card has already been flipped")
+
+		outstring = ""
+
+
+		c = 0
+		for instance in board:
+			outstring += instance
+			c += 1
+			if c > 5:
+				c = 0
+				outstring += "\n"
+		await client.send_message(message.channel, outstring)
+
+	elif message.content.lower().startswith("~spy"):
+		masterList = ""
+		for each in range(0, len(board)):
+			if wordStates[each] == 0:
+				 masterList += board[each] + " is red"
+			if wordStates[each] == 1:
+				 masterList += board[each] + " is blue"
+			if wordStates[each] == 0:
+				 masterList += board[each] + " is civilian"
+			if wordStates[each] == 0:
+				 masterList += board[each] + " is assassin"
+			wordList += "\n"
+
+			client.send_message(message.author, masterList)
+
+
 	#1 in 50 chance to react with a random server-specific image
 	if random.randint(0,50) == 0:
-		list = []		
+		list = []
 		for e in client.get_all_emojis():
 			print(str(e))
 			list.append(e)
 		await client.add_reaction(message, list[random.randint(0,len(list)-1)])
 
-        
 
 
 #Discord event listener.  When  we recieve a message pass it to public() which handles that stuff
 @client.event
-async def on_message(message):	
+async def on_message(message):
 	await public(message)
 
 #Discord event listener.  When a reaction is added, the bot adds that same reaction.  We also update that user's emoji count
 @client.event
-async def on_reaction_add(reaction, user):	
+async def on_reaction_add(reaction, user):
 	await client.add_reaction(reaction.message, reaction.emoji)
 	for u in users:
 		if u.ID == user.id:
@@ -241,7 +331,7 @@ async def on_reaction_add(reaction, user):
 
 #Discord event listener.  If a reaction is removed from a post we update that user's emoji count.  If that was the last reaction we also remove the bot's
 @client.event
-async def on_reaction_remove(reaction, user):	
+async def on_reaction_remove(reaction, user):
 	if reaction.count == 1:
 		await client.remove_reaction(reaction.message, reaction.emoji, client.user)
 	for u in users:
@@ -257,31 +347,30 @@ async def on_ready():
 	global calendar
 	global users
 
-
 #uggh can I just leave the voice parts uncommented?  no?  shit.
 async def voiceCmd(message):
 
 	#there's going to be some light fuckery going on so lets make it so we don't have to worry about scope.
 	global voice
-	global player	
+	global player
 	global _in_voice
 	global q
 	global _skip_flag
 
 	#this is slightly redundant, since the voice commands should never even get this function called if the user isn't in a channel.
 	#we just double check to make sure they are in fact in voice since things break if they aren't
-	if message.author.voice_channel != None:		
+	if message.author.voice_channel != None:
 		#first check if we already have a player make...
 		if not player == None:
 			#...if we do, then what we do is add their song to the front of the queue and skip the current song
 			#make a temp queue, add their message,
-			tq = queue.Queue(maxsize = 0)		
+			tq = queue.Queue(maxsize = 0)
 			tq.put(message)
-			
+
 			#then copy over the contents of the old queue to the temp queue
 			while not q.empty():
 				tq.put(q.get())
-		
+
 			#and set the old queue to be equal to the temp queue.
 			q = tq
 
@@ -303,7 +392,7 @@ async def voiceCmd(message):
 	elif not _in_voice:
 		await client.send_message(message.channel, "Please be in a voice channel to make requests")
 		return 0
-	
+
 	if len(message.content.split(" ")) == 0:
 		await client.send_message(message.channel, "I don't think you gave me a song to play?  Please try again.")
 		return 0
@@ -343,19 +432,19 @@ async def play(message):
 	global voice
 	global _in_voice
 	ours = message
-	
+
 	#Recall that voiceCmd() returns the size of the queue.  As long as the queue has something in it we play the next song.
 	#Actual process looks like this:  get song from queue ==> pass song to voiceCmd(), waiting for it to finish ==> check if we need to repeat
 	while (await voiceCmd(ours)) > 0:
-		ours = q.get() 
+		ours = q.get()
 
 	#if the queue if finally empty we are done and need to disconnect.
 	await voice.disconnect()
 	_in_voice = False
-	
+
 async def ffmpeg(message):
 	global voice
-	global player	
+	global player
 	#do some good ol fashioned unix based black magic to jump in voice and then download and start a song.
 	if player is not None:
 		player.stop()
@@ -376,10 +465,10 @@ async def rmq(message):
 		if not (t.content.lower().split(" ",1)[1] == message.content.lower().split(" ",1)[1]):
 			tq.put(t)
 		time.sleep(1)
-		
+
 	#and set the old queue to be equal to the temp queue.
 	q = tq
 
 #actually starts the bot.  client.run() gets passes my client secret, which is basically botty mcbotface's soul
-print("Launching bot...")	
-client.run(BOT_SECRET)
+print("Launching bot...")
+client.run("NDQzOTgzNTA0MzQ3MjM0MzE0.Dt4bUQ.uaAiExgl6FI5WspyjHh57RB2dFg")
